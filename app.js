e40b446343dc02da7e8fcdc2c02aefa6090e2964
Shark Tiến <mineshark15@gmail.com>
2023-07-27 12:38:30 +0700

    fix bug

--- app.js
+++ app.js
@@ -518,7 +518,7 @@ const addSongItem = (number, i, songList_id, IdItem) => {
     li = document.createElement("li");
     li.classList.add(IdItem);
     li.innerHTML = `
-    <button class="run" onclick="activeMusic(${i})"><svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg></button>
+    <button class="run" onclick="activeMusicandblur(${i}); this.blur();"><svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg></button>
     <span>${number}</span>
     <div>
     <img src=${songs[i].cover}>
@@ -531,6 +531,11 @@ const addSongItem = (number, i, songList_id, IdItem) => {
     songList.appendChild(li);
     };  
 
+const activeMusicandblur = (i) => {
+    activeMusic(i);
+    this.blur()
+}
+    
 num = 0;
 for (let i = 0; i < songs.length; i++) {
     num += 1;
